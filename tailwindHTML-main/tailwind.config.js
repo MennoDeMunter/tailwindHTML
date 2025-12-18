104c5953bf459ebc3891007cd335af13d1942d7b
MennoDeMunter <minibotser2.0@hotmail.com>
2025-12-18 16:51:41 +0100

    Add files via upload

--- tailwindHTML-main/tailwind.config.js
+++ tailwindHTML-main/tailwind.config.js
@@ -1,5 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    './**/*.html',
+    './src/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'p-dark': '#3B2B20',
+        'p-mid': '#8B6B5A',
+        'p-light': '#F3EFE9',
+        'p-acc': '#B86B56',
+        'start-bg': '#3B2B20'
+      }
+    }
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
